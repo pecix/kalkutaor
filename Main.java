34a0b47fd76128d559a59a69331b51edec32e65c
Artur Petrusiewicz <pecixx@gmail.com>
2024-06-15 12:07:00 +0200

    test messeage

--- Main.java
+++ Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("test");
-        System.out.println("test4");
+        System.out.println("test5");
     }
 }
